f7820466985397cf538497d55de1554b2bc3cb3a
tgcbraun <thiago@gcbraun.com.br>
2015-03-07 11:48:06 -0300

    Committed by Udacity

---
+++ Pitch Perfect/Pitch Perfect/RecordedAudio.swift
@@ -0,0 +1,16 @@
+//
+//  RecordedAudio.swift
+//  Pitch Perfect
+//
+//  Created by Thiago Graça Couto Braun on 3/7/15.
+//  Copyright (c) 2015 GCBraun. All rights reserved.
+//
+
+import Foundation
+
+class RecordedAudio: NSObject{
+    
+    var filePathUrl: NSURL!
+    var title: String!
+    
+}
